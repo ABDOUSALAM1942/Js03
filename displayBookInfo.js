583fa28b9e0cbd5707f3a3362d9941d7417af7a1
Abdoul Aziz Yatagan <124500030+ABDOUSALAM1942@users.noreply.github.com>
2023-02-15 22:03:50 +0100

    Add files via upload

---
+++ displayBookInfo.js
@@ -0,0 +1,11 @@
+let book = {
+    Titre: "L'Enfant Noir",
+    Auteur: "Camara Laye",
+    année: 1953,
+};
+function displayBookInfo(book) {
+    console.log("Titre:", book.Titre);
+    console.log("Auteur:", book.Auteur);
+    console.log("année:", book.année);
+}
+displayBookInfo(book);
